69011340bf85798c3c64eb6c76a36b214901a0d6
syarna <syarnasavitri66@gmail.com>
2025-02-13 20:18:20 +0700

    create ProductController.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public String createProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    @PostMapping
+    public String createProduct(@ModelAttribute Product product, Model model) {
+        service.create(product);
+        return "redirect:list";
+    }
+
+    @PostMapping
+    public String productListPage(Model model) {
+        List<Product> allProducts = service.findAll();
+        model.addAttribute("products", allProducts);
+        return "productList";
+    }
+}
